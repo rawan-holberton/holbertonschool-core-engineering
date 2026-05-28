b021051f252360be5c0176499c649c6271c17066
Rawan <rawansafouan@gmail.com>
2026-05-28 10:20:05 +0200

    erasing the last empty line requeried by exercise description even thought the checker don't accept it

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -6,4 +6,3 @@ def write_file(filename="", text=""):
     """Write a string to a text file and return character count."""
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
-
