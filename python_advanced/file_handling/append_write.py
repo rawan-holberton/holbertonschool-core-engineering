76fd20397a311e661ff99693318fe19dd29bc754
Rawan <rawansafouan@gmail.com>
2026-05-28 10:13:54 +0200

    trying to see if the last blank line is what cause the requirement fail by the checker

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -6,4 +6,3 @@ def append_write(filename="", text=""):
     """Append a string to a text file and return character count."""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
-
