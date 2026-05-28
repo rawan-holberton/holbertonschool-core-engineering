acabe3c50801ed89f1ed29084c9c80cfc29854c7
Rawan <rawansafouan@gmail.com>
2026-05-28 10:03:25 +0200

    modified file after checker correction n°1

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Module for writing text to a UTF-8 file."""
 
 
@@ -6,3 +6,4 @@ def write_file(filename="", text=""):
     """Write a string to a text file and return character count."""
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
+
