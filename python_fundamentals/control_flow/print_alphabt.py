cad61bb3f2129a216f02a946838a34334f687f6a
Rawan <rawansafouan@gmail.com>
2026-07-02 09:44:01 +0200

    correction: change the output required

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-alphabet = ""
-
-for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('e') and letter != ord('q'):
-        alphabet += chr(letter)
-
-print("{}".format(alphabet))
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
