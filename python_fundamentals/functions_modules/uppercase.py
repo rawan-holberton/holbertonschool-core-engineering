1fbdf6c87acb20df5fa7602e242bf26663a8e399
Rawan <rawansafouan@gmail.com>
2026-07-02 10:16:01 +0200

    adding task 1 project Python - Functions & Modularity

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    result = ""
+
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+
+    print("{}".format(result))
