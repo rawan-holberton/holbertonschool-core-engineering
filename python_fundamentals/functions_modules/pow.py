40d7e6e977e5fb5d94d222371d8c20d6c5057ca7
Rawan <rawansafouan@gmail.com>
2026-07-02 12:46:02 +0200

    adding gestion des exposant negative

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -3,7 +3,12 @@
 def pow(a, b):
     result = 1
 
-    for _ in range(b):
-        result *= a
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result *= a
+        result = 1 / result
 
     return result
