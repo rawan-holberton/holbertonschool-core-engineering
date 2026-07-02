e772bcbb8f0ec1c72f5ef490580c72363ed5ee79
Rawan <rawansafouan@gmail.com>
2026-07-02 09:40:35 +0200

    correction: use one print

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+
+alphabet = ""
+
 for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('q') and letter != ord('e'):
-        print("{}".format(chr(letter)), end="")
-print()
+    if letter != ord('e') and letter != ord('q'):
+        alphabet += chr(letter)
+
+print("{}".format(alphabet))
