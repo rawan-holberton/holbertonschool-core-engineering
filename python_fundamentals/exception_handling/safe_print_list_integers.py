18fdd534abc93a1402a1e7b664867cbea24893fc
Rawan <rawansafouan@gmail.com>
2026-07-02 13:26:30 +0200

    adding task 2

---
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for i in range(x):
+        try:
+            value = my_list[i]
+            print("{:d}".format(value), end="")
+            count += 1
+        except (IndexError, TypeError, ValueError):
+            continue
+
+    print()
+    return count
