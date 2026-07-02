4250edf3b0b014e1128cd718b15ce0af5a15ff32
Rawan <rawansafouan@gmail.com>
2026-07-02 13:18:10 +0200

    adding task 7

---
+++ python_fundamentals/core_data_structures/best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    best_key = None
+    best_value = None
+
+    for key, value in a_dictionary.items():
+        if best_value is None or value > best_value:
+            best_value = value
+            best_key = key
+
+    return best_key
