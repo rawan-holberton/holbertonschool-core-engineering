b89f3d45d5c267d8401d07ba0341b80d0c4baf7f
Rawan <rawansafouan@gmail.com>
2026-07-02 15:53:11 +0200

    correct the loop

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+"""Module that safely prints integers from a list."""
+
 
 def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x integers of a list safely."""
     count = 0
 
     for i in range(x):
         try:
-            value = my_list[i]
-            print("{:d}".format(value), end="")
-            count += 1
-        except (IndexError, TypeError, ValueError):
-            continue
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        except IndexError:
+            break
 
     print()
     return count
