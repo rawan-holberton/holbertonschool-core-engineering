aa5caae0bc10651edd18f20f995b657bfdb2d863
Rawan <rawansafouan@gmail.com>
2026-07-02 09:48:24 +0200

    adding task 5

---
+++ python_fundamentals/control_flow/print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 0 and j == 1:
+            print("{}{}".format(i, j), end="")
+        else:
+            print(", {}{}".format(i, j), end="")
+
+print()
